e757d2e916ccdf55a239dffe47a771b8f393b02c
tsutterley <tsutterl@uw.edu>
2024-06-26 09:38:46 -0700

    [WIP] hydrodynamic modeling for tides
    [WIP] simplify shift indice creation
    [WIP] always use Dirichlet for N-S bounds
    [WIP] add boundary condition enforcement
    [WIP] start adding response method and orthotides

--- pyTMD/solve/__init__.py
+++ pyTMD/solve/__init__.py
@@ -3,3 +3,7 @@ Solve functions for estimating ocean tides
 """
 
 from .constants import *
+from .grid import grid
+from .model import model
+from .forward import forward
+from .response import response
