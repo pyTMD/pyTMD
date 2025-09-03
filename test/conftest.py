9069a272da694e59997d6fad9dc3b4ce7521f0c3
Tyler Sutterley <tsutterl@uw.edu>
2025-09-03 09:54:27 -0700

    test: add check if running on GitHub Actions
    https://github.com/openjournals/joss-reviews/issues/8566#issuecomment-3097482521

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,21 @@
 import pytest
+import inspect
+import pathlib
+
+# current file path
+filename = inspect.getframeinfo(inspect.currentframe()).filename
+filepath = pathlib.Path(filename).absolute().parent
 
 def pytest_addoption(parser):
+    parser.addoption("--directory", action="store", help="Directory for test data", default=filepath, type=pathlib.Path)
     parser.addoption("--aws-access", action="store", help="AWS Access Key ID")
     parser.addoption("--aws-secret", action="store", help="AWS Secret Key")
     parser.addoption("--aws-region", action="store", help="AWS Region Name")
+
+@pytest.fixture(scope="session")
+def directory(request):
+    """ Returns Data Directory """
+    return request.config.getoption("--directory")
 
 @pytest.fixture(scope="session")
 def aws_access_key_id(request):
