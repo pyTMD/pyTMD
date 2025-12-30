a298c890997bf72d8325fa690f586b28a0d10a60
Tyler Sutterley <tsutterl@uw.edu>
2025-12-29 20:00:20 -0800

    refactor: ruff formatting

--- pyTMD/solve/response.py
+++ pyTMD/solve/response.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-u"""
+"""
 response.py
 Written by Tyler Sutterley (11/2024)
 Routines for estimating tidal constituents using the response method
@@ -12,8 +12,8 @@ PYTHON DEPENDENCIES:
         https://scipy.org
 
 PROGRAM DEPENDENCIES:
-    arguments.py: loads nodal corrections for tidal constituents
     astro.py: computes the basic astronomical mean longitudes
+    constituents.py: calculates constituent parameters and nodal arguments
     math.py: special mathematical functions
     spatial.py: utilities for spatial operations
 
@@ -26,27 +26,23 @@ from __future__ import annotations
 import numpy as np
 import scipy.linalg
 import scipy.optimize
-import pyTMD.arguments
+import pyTMD.constituents
 import pyTMD.astro
 import pyTMD.math
 import pyTMD.spatial
 
-__all__ = [
-    'response',
-    '_gravitational',
-    '_radiational',
-    '_kappa'
-]
+__all__ = ["response", "_gravitational", "_radiational", "_kappa"]
+
 
 def response(
-        t: np.ndarray,
-        lon: np.ndarray,
-        lat: np.ndarray,
-        ht: np.ndarray,
-        constituents: str | list | np.ndarray,
-        corrections: str = 'OTIS',
-        **kwargs: dict
-    ):
+    t: np.ndarray,
+    lon: np.ndarray,
+    lat: np.ndarray,
+    ht: np.ndarray,
+    constituents: str | list | np.ndarray,
+    corrections: str = "OTIS",
+    **kwargs: dict,
+):
     """
     Estimate tidal constituents using the response method
     :cite:p:`Munk:1966go,Groves:1975ky,Zetler:1975uv,Cartwright:1990ck`
@@ -60,7 +56,7 @@ def response(
     lat: np.ndarray
         latitude coordinates
     ht: np.ndarray
-        elevation time series (meters)
+        input time series (elevations)
     constituents: str, list or np.ndarray
         tidal constituent ID(s)
     corrections: str, default 'OTIS'
@@ -76,55 +72,67 @@ def response(
         phase of each harmonic constant (degrees)
     """
     # default keyword arguments
-    kwargs.setdefault('ephemerides', 'approximate')
+    kwargs.setdefault("a_axis", 6378137.0)
+    kwargs.setdefault("flat", 1.0 / 298.257223563)
+    kwargs.setdefault("ephemerides", "approximate")
     # check if input constituents is a string
     if isinstance(constituents, str):
         constituents = [constituents]
-    # number of time values and constituents
-    nt = len(np.atleast_1d(t))
+    # verify shape of variables
+    t = np.ravel(t)
+    lon = np.ravel(lon)
+    lat = np.ravel(lat)
+    ht = np.ravel(ht)
+    # reduce variables to finite values
+    if not np.isfinite(ht).all():
+        (valid,) = np.nonzero(np.isfinite(t) & np.isfinite(ht))
+        t = t[valid]
+        lon = lon[valid]
+        lat = lat[valid]
+        ht = ht[valid]
+    # check that there are enough values for a time series fit
+    nt = len(t)
     nc = len(constituents)
-    # verify dimensions
-    singular_values = (np.ndim(t) == 0)
-    t = np.atleast_1d(t).flatten()
-    lon = np.atleast_1d(lon).flatten()
-    lat = np.atleast_1d(lat).flatten()
     # assert dimensions
-    assert len(t) == len(lat), 'coordinates must have the same dimensions'
-    assert len(lon) == len(lat), 'coordinates must have the same dimensions'
+    assert len(t) == len(lat), "coordinates must have the same dimensions"
+    assert len(lon) == len(lat), "coordinates must have the same dimensions"
     # check that the number of time values matches the number of height values
-    if (nt != len(np.atleast_1d(ht))):
-        raise ValueError('Dimension mismatch between input variables')
-    # degrees to radians
-    dtr = np.pi/180.0
+    if nt != len(np.atleast_1d(ht)):
+        raise ValueError("Dimension mismatch between input variables")
+    # convert lon, lat to ECEF coordinates
+    X, Y, Z = pyTMD.spatial.to_cartesian(
+        lon, lat, a_axis=kwargs["a_axis"], flat=kwargs["flat"]
+    )
+    XYZ = np.c_[X, Y, Z]
+    # geocentric latitude (radians)
+    theta = np.pi / 2.0 - np.arctan(
+        XYZ[:, 2] / np.sqrt(XYZ[:, 0] ** 2.0 + XYZ[:, 1] ** 2.0)
+    )
+    # longitude (radians)
+    phi = np.arctan2(XYZ[:, 1], XYZ[:, 0])
     # solar ephemerides (convert time to Modified Julian Days)
     SXYZ = pyTMD.astro.solar_ecef(t + 48622.0, **kwargs)
     # lunar ephemerides (convert time to Modified Julian Days)
     LXYZ = pyTMD.astro.lunar_ecef(t + 48622.0, **kwargs)
+    # spherical harmonics for degree and order
+    Ylm, _ = pyTMD.math.sph_harm(2, theta, phi, m=0, phase=0.0)
+    # gravitational potentials
+    US, UL = _gravitational(XYZ, SXYZ, LXYZ, l=2, **kwargs)
+    # radiational function
+    RS = _radiational(XYZ, SXYZ, l=2, **kwargs)
+
 
 def _gravitational(
-        t: float | np.ndarray,
-        lon: np.ndarray,
-        lat: np.ndarray,
-        ht: np.ndarray,
-        SXYZ: np.ndarray,
-        LXYZ: np.ndarray,
-        l: int,
-        **kwargs: dict
-    ):
+    XYZ: np.ndarray, SXYZ: np.ndarray, LXYZ: np.ndarray, l: int, **kwargs
+):
     """
     Estimate gravitational tides using the response method
     :cite:p:`Munk:1966go`
 
     Parameters
     ----------
-    t: np.ndarray
-        Time (days relative to January 1, 1992)
-    lon: np.ndarray
-        longitude coordinates
-    lat: np.ndarray
-        latitude coordinates
-    ht: np.ndarray
-        elevation time series (meters)
+    XYZ: np.ndarray
+        Earth-centered Earth-fixed coordinates of the observation points (meters)
     SXYZ: np.ndarray
         Earth-centered Earth-fixed coordinates of the sun (meters)
     LXYZ: np.ndarray
@@ -134,65 +142,75 @@ def _gravitational(
 
     Returns
     -------
-    amp: np.ndarray
-        amplitude of each harmonic constant (meters)
-    phase: np.ndarray
-        phase of each harmonic constant (degrees)
+    solar_grav: np.ndarray
+        gravitatinal potential of the sun
+    lunar_grav: np.ndarray
+        gravitatinal potential of the moon
     """
     # default keyword arguments
-    kwargs.setdefault('a_axis', 6378137.0)
-    kwargs.setdefault('flat', 1.0/298.257223563)
-    kwargs.setdefault('omega', 7.2921151467e-5)
-    kwargs.setdefault('AU', 1.495978707e11)
-    kwargs.setdefault('LD', 3.84399e8)
+    kwargs.setdefault("a_axis", 6378137.0)
+    kwargs.setdefault("flat", 1.0 / 298.257223563)
+    kwargs.setdefault("omega", 7.2921151467e-5)
+    kwargs.setdefault("AU", 1.495978707e11)
+    kwargs.setdefault("LD", 3.84399e8)
     # mass ratios between earth and sun/moon
-    kwargs.setdefault('mass_ratio_solar', 332946.0482)
-    kwargs.setdefault('mass_ratio_lunar', 0.0123000371)
-    # degrees to radians
-    dtr = np.pi/180.0
+    kwargs.setdefault("mass_ratio_solar", 332946.0482)
+    kwargs.setdefault("mass_ratio_lunar", 0.0123000371)
     # average radius of the earth
-    rad_e = kwargs['a_axis']*(1.0 - kwargs['flat'])**(1.0/3.0)
+    rad_e = kwargs["a_axis"] * (1.0 - kwargs["flat"]) ** (1.0 / 3.0)
     # solar and lunar radii from ephemerides
-    solar_radius = np.sqrt(SXYZ[:,0]**2 + SXYZ[:,1]**2 + SXYZ[:,2]**2)
-    lunar_radius = np.sqrt(LXYZ[:,0]**2 + LXYZ[:,1]**2 + LXYZ[:,2]**2)
+    solar_radius = np.sqrt(SXYZ[:, 0] ** 2 + SXYZ[:, 1] ** 2 + SXYZ[:, 2] ** 2)
+    lunar_radius = np.sqrt(LXYZ[:, 0] ** 2 + LXYZ[:, 1] ** 2 + LXYZ[:, 2] ** 2)
+    # geocentric latitude (radians)
+    theta = np.arctan(XYZ[:, 2] / np.sqrt(XYZ[:, 0] ** 2.0 + XYZ[:, 1] ** 2.0))
+    # longitude (radians)
+    phi = np.arctan2(XYZ[:, 1], XYZ[:, 0])
     # convert solar and lunar ephemerides from ECEF to zenith angle
-    solar_zenith = pyTMD.spatial.to_zenith(SXYZ[:,0], SXYZ[:,1], SXYZ[:,2],
-        lon, lat, **kwargs)
-    lunar_zenith = pyTMD.spatial.to_zenith(LXYZ[:,0], LXYZ[:,1], LXYZ[:,2],
-        lon, lat, **kwargs)
+    solar_zenith = pyTMD.spatial.to_zenith(
+        SXYZ[:, 0],
+        SXYZ[:, 1],
+        SXYZ[:, 2],
+        np.degrees(phi),
+        np.degrees(theta),
+        **kwargs,
+    )
+    lunar_zenith = pyTMD.spatial.to_zenith(
+        LXYZ[:, 0],
+        LXYZ[:, 1],
+        LXYZ[:, 2],
+        np.degrees(phi),
+        np.degrees(theta),
+        **kwargs,
+    )
     # associated Legendre functions of zenith angle for degree l
-    solar_legendre = pyTMD.math.legendre(l, np.cos(dtr*solar_zenith))
-    lunar_legendre = pyTMD.math.legendre(l, np.cos(dtr*lunar_zenith))
+    solar_legendre, _ = pyTMD.math.legendre(l, np.cos(np.radians(solar_zenith)))
+    lunar_legendre, _ = pyTMD.math.legendre(l, np.cos(np.radians(lunar_zenith)))
     # k values from Munk and Cartwright (1966)
-    solar_k = rad_e*kwargs['mass_ratio_solar']*(rad_e/solar_radius)**(l+1)
-    lunar_k = rad_e*kwargs['mass_ratio_lunar']*(rad_e/lunar_radius)**(l+1)
+    solar_k = (
+        rad_e * kwargs["mass_ratio_solar"] * (rad_e / solar_radius) ** (l + 1)
+    )
+    lunar_k = (
+        rad_e * kwargs["mass_ratio_lunar"] * (rad_e / lunar_radius) ** (l + 1)
+    )
     # gravitational potential of the sun and moon
-    solar_grav = solar_k*solar_legendre*(kwargs['AU']/solar_radius)**(l+1)
-    lunar_grav = lunar_k*lunar_legendre*(kwargs['LD']/lunar_radius)**(l+1)
+    solar_grav = (
+        solar_k * solar_legendre * (kwargs["AU"] / solar_radius) ** (l + 1)
+    )
+    lunar_grav = (
+        lunar_k * lunar_legendre * (kwargs["LD"] / lunar_radius) ** (l + 1)
+    )
+    return (solar_grav, lunar_grav)
 
-def _radiational(
-        t: float | np.ndarray,
-        lon: np.ndarray,
-        lat: np.ndarray,
-        ht: np.ndarray,
-        SXYZ: np.ndarray,
-        l: int,
-        **kwargs: dict
-    ):
+
+def _radiational(XYZ: np.ndarray, SXYZ: np.ndarray, l: int, **kwargs):
     """
     Estimate radiational tides using the response method
     :cite:p:`Munk:1966go`
 
     Parameters
     ----------
-    t: np.ndarray
-        Time (days relative to January 1, 1992)
-    lon: np.ndarray
-        longitude coordinates
-    lat: np.ndarray
-        latitude coordinates
-    ht: np.ndarray
-        elevation time series (meters)
+    XYZ: np.ndarray
+        Earth-centered Earth-fixed coordinates of the observation points (meters)
     SXYZ: np.ndarray
         Earth-centered Earth-fixed coordinates of the sun (meters)
     l: int
@@ -200,32 +218,44 @@ def _radiational(
 
     Returns
     -------
-    amp: np.ndarray
-        amplitude of each harmonic constant (meters)
-    phase: np.ndarray
-        phase of each harmonic constant (degrees)
+    solar_rad: np.ndarray
+        radiation function of the sun
     """
     # default keyword arguments
-    kwargs.setdefault('a_axis', 6378137.0)
-    kwargs.setdefault('solar_constant', 1380.0)
-    kwargs.setdefault('AU', 1.495978707e11)
-    # degrees to radians
-    dtr = np.pi/180.0
+    kwargs.setdefault("a_axis", 6378137.0)
+    kwargs.setdefault("solar_constant", 1380.0)
+    kwargs.setdefault("AU", 1.495978707e11)
     # Earth equatorial radius in AU (~1/23455)
     # defined as parallax in Munk and Cartwright (1966)
-    xi = kwargs['a_axis']/kwargs['AU']
+    xi = kwargs["a_axis"] / kwargs["AU"]
     # kappa value (Munk and Cartwright, 1966)
     kappa = _kappa(l, xi)
     # solar radius from ephemerides
-    solar_radius = np.sqrt(SXYZ[:,0]**2 + SXYZ[:,1]**2 + SXYZ[:,2]**2)
+    solar_radius = np.sqrt(SXYZ[:, 0] ** 2 + SXYZ[:, 1] ** 2 + SXYZ[:, 2] ** 2)
+    # geocentric latitude (radians)
+    theta = np.arctan(XYZ[:, 2] / np.sqrt(XYZ[:, 0] ** 2.0 + XYZ[:, 1] ** 2.0))
+    # longitude (radians)
+    phi = np.arctan2(XYZ[:, 1], XYZ[:, 0])
     # convert solar ephemerides from ECEF to zenith angle
-    solar_zenith = pyTMD.spatial.to_zenith(SXYZ[:,0], SXYZ[:,1], SXYZ[:,2],
-        lon, lat, **kwargs)
+    solar_zenith = pyTMD.spatial.to_zenith(
+        SXYZ[:, 0],
+        SXYZ[:, 1],
+        SXYZ[:, 2],
+        np.degrees(phi),
+        np.degrees(theta),
+        **kwargs,
+    )
     # create radiation function
-    S = np.copy(kwargs['solar_constant'])
-    R = S*(kwargs['AU']/solar_radius)*kappa*np.cos(dtr*solar_zenith)**l
+    solar_rad = (
+        kwargs["solar_constant"]
+        * (kwargs["AU"] / solar_radius)
+        * kappa
+        * np.cos(np.radians(solar_zenith)) ** l
+    )
     # radiation is only valid during the day
-    R = np.where(solar_zenith < 0.5*np.pi, R, 0.0)
+    solar_rad = np.where(solar_zenith < 0.5 * np.pi, solar_rad, 0.0)
+    return solar_rad
+
 
 def _kappa(l: int, xi: float) -> float:
     """
@@ -244,7 +274,7 @@ def _kappa(l: int, xi: float) -> float:
         kappa value for the given degree
     """
     kappa = np.zeros((4))
-    kappa[0] = 1.0/4.0 + xi/6.0
-    kappa[1] = 1.0/2.0 + 3.0*xi/8.0
-    kappa[2] = 5.0/16.0 + xi/3.0
+    kappa[0] = 1.0 / 4.0 + xi / 6.0
+    kappa[1] = 1.0 / 2.0 + 3.0 * xi / 8.0
+    kappa[2] = 5.0 / 16.0 + xi / 3.0
     return kappa[l]
