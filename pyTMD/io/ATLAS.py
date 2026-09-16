06f8df61bf579b98db033325c8539507715b5a68
Robbi Bishop-Taylor <Robbi.BishopTaylor@ga.gov.au>
2026-09-17 06:09:52 +1000

    fix: mask ATLAS 0 values to `NaN` (#624)
    * set `drop` to `False` to prevent problems when merging

--- pyTMD/io/ATLAS.py
+++ pyTMD/io/ATLAS.py
@@ -298,6 +298,8 @@ def open_atlas_dataset(
         ds.coords["x"] = tmp["lon_v"]
         ds.coords["y"] = tmp["lat_v"]
         ds[con].attrs["units"] = tmp["vRe"].attrs.get("units")
+    # set complex zero values to nan
+    ds = ds.where(ds[con] != 0, None, drop=False)
     # swap dimension names
     ds = ds.swap_dims(dict(nx="x", ny="y"))
     # add attributes
